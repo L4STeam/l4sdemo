d72f458044590c3596e04f51d8fae2f775b4b5f6
Olivier Tilmans <olivier.tilmans@nokia-bell-labs.com>
2019-07-01 19:53:33 +0200

    Fix iproute install script

--- iproute2-addons/build.sh
+++ iproute2-addons/build.sh
@@ -6,20 +6,23 @@ TCDIR="${DEST}/tc"
 
 echo "Building iproute2"
 
-rm -rf "$DEST"
+if [ ! -d "$DEST" ]; then
 git clone \
     --depth 1 --single-branch \
     -b $(uname -r | awk -F '.' '{ printf "v%d.%d.0", $1, $2 }') \
     git://git.kernel.org/pub/scm/network/iproute2/iproute2.git "$DEST"
-
+    for qdisc in *.c; do
+        cp "$qdisc" "${TCDIR}"
+        sed -i "/^TCMODULES :=/a TCMODULES += ${qdisc/%.c/.o}" "${TCDIR}/Makefile"
+    done
+fi
 for qdisc in *.c; do
     cp "$qdisc" "${TCDIR}"
-    sed -i "/^TCMODULES :=/a TCMODULES += ${qdisc/%.c/.o}" "${TCDIR}/Makefile"
 done
 
 cat "${HERE}/../kernel_modules/sch_dualpi2/compat-pkt_sched.h" >> "${DEST}/include/uapi/linux/pkt_sched.h"
 
-pushd "${TCDIR}"
+pushd "${DEST}"
 ./configure
 make -j$(nproc)
 popd
