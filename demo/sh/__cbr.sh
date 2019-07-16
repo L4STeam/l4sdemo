cae0ed6bda8ae49e8f3dd7c86e0d779aa77a5a2f
Olivier Tilmans <olivier.tilmans@nokia-bell-labs.com>
2019-07-16 16:26:57 +0200

    Spawn iperf sinks as well

--- demo/sh/__cbr.sh
+++ demo/sh/__cbr.sh
@@ -10,7 +10,9 @@ source "${HERE}/__ssh_lib.sh"
 declare -i rate=$1
 
 do_ssh ${SERVER} 'killall iperf' &> /dev/null
+do_ssh ${CLIENT} 'killall iperf' &> /dev/null
 if [[ "$rate" > 0 ]]; then
     SSH_FLAGS="-f"
+    do_ssh ${CLIENT}  "iperf -Ds"
     do_ssh ${SERVER}  "iperf -c ${CLIENT} -u -t 0 -b ${rate}m" &
 fi
