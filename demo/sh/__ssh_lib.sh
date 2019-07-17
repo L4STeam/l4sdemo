4a4d7850848f463c4680d0353444f4cc7ac5e62d
Olivier Tilmans <olivier.tilmans@nokia-bell-labs.com>
2019-07-17 22:13:14 +0200

    Improve remote ssh logging

--- demo/sh/__ssh_lib.sh
+++ demo/sh/__ssh_lib.sh
@@ -5,17 +5,19 @@ function do_ssh()
 {
     local hname=$1
     shift 1
-    echo "[$hname;$SSH_FLAGS] $@"
     cmd=ssh
     if [ "x$SSH_FLAGS" != "x" ]; then
         cmd="$cmd $SSH_FLAGS"
     fi
-    $cmd \
+    echo "$cmd $hname $@"
+    $cmd -n \
         -oControlMaster=auto \
         -oControlPath="/tmp/ssh_${hname}.sock" \
         -oControlPersist=300s \
         -oConnectTimeout=5 \
-        "$hname" "$@"
+        "$hname" "$@" \
+        > >(sed "s/^/[$hname] /") \
+        2> >(sed "s/^/E[${hname}] /" >&2)
 }
 
 function clean_ssh()
