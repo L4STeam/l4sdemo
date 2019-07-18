e17b75b38598dbeecf0cb18db18799a4e8eba3c8
Olivier Tilmans <olivier.tilmans@nokia-bell-labs.com>
2019-07-18 09:48:27 +0200

    Fix ehttool param

--- run_demo.sh
+++ run_demo.sh
@@ -3,8 +3,8 @@
 source $(dirname $0)/environment.sh
 
 sudo sysctl -qw net.ipv4.ip_forward=1
-sudo ethtool -k $REV_IFACE gro off
-sudo ethtool -k $REV_IFACE gso off
-sudo ethtool -k $REV_IFACE tso off
+sudo ethtool -K $REV_IFACE gro off
+sudo ethtool -K $REV_IFACE gso off
+sudo ethtool -K $REV_IFACE tso off
 
 ./demo/L4SDemo
