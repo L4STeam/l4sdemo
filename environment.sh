8739480ec82b9f5afd736b5c92b27d73eac11866
monta <munt_jaber@outlook.com>
2020-03-04 13:41:55 +0100

    Adjusted environment.sh for debugging

--- environment.sh
+++ environment.sh
@@ -1,22 +1,45 @@
 # Source this file
 
-# The interface facing the clients
-export IFACE="enp101s0f0"
-# The interface facing the servers
-export REV_IFACE="enp101s0f0"
-# The IP prefix of the servers
-export SRC_NET="10.0.0.0/24"
-# Client and servers addresses
-export SERVER_A="10.0.0.1"
-# export SERVER_B="192.168.10.114"
-export CLIENT_A="10.0.0.2"
-export CLIENT_B="10.0.0.3"
-# The interface on both clients connected to the aqm/router (to apply mixed rtt)
-export CLIENT_A_IFACE="enp2s0f0"
-export CLIENT_B_IFACE="enp1s0f0"
-# Server interfaces that might need to be tuned (e.g., offload, ...)
-export SERVER_A_IFACE="enp1s0f0"
-# export SERVER_B_IFACE="eth0"
+if [ -z "$1"]; then
+    # The interface facing the clients
+    export IFACE="enp3s0"
+    # The interface facing the servers
+    export REV_IFACE="enp3s0"
+    # The IP prefix of the servers
+    export SRC_NET="192.168.60.0/24"
+    # Client and servers addresses
+    export SERVER_A="192.168.60.124"
+    # export SERVER_B="192.168.10.114"
+    export CLIENT_A="192.168.60.235"
+    # export CLIENT_B="10.0.0.3"
+    # The interface on both clients connected to the aqm/router (to apply mixed rtt)
+    export CLIENT_A_IFACE="enp2s0f1"
+    # export CLIENT_B_IFACE="enp1s0f0"
+    # Server interfaces that might need to be tuned (e.g., offload, ...)
+    export SERVER_A_IFACE="eno1"
+    # export SERVER_B_IFACE="eth0"
+
+    elif [ "$1" = "debug" ]; then
+    # The interface facing the clients
+    export IFACE="tun0"
+    # The interface facing the servers
+    export REV_IFACE="tun0"
+    # The IP prefix of the servers
+    export SRC_NET="192.168.60.0/24"
+    # Client and servers addresses
+    export SERVER_A="192.168.60.124"
+    # export SERVER_B="192.168.10.114"
+    export CLIENT_A="192.168.60.235"
+    # export CLIENT_B="10.0.0.3"
+    # The interface on both clients connected to the aqm/router (to apply mixed rtt)
+    export CLIENT_A_IFACE="enp2s0f1"
+    # export CLIENT_B_IFACE="enp1s0f0"
+    # Server interfaces that might need to be tuned (e.g., offload, ...)
+    export SERVER_A_IFACE="eno1"
+    # export SERVER_B_IFACE="eth0"
+fi
+
+
 # Ports used to send web request completion times to the AQM node
 export PORT_A=11000
 export PORT_B=11001
