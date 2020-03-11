17e1a226243a3306c22c4ad93671fe2b672a4ded
Muntazar Al-Jaber <munt_jaber@outlook.com>
2020-03-11 04:08:49 +0100

    Missing max() declaration caused compilation to fail and default incorrectly to fmax()

--- iproute2-addons/build.sh
+++ iproute2-addons/build.sh
@@ -7,10 +7,10 @@ TCDIR="${DEST}/tc"
 echo "Building iproute2"
 
 if [ ! -d "$DEST" ]; then
-git clone \
-    --depth 1 --single-branch \
-    -b $(uname -r | awk -F '.' '{ printf "v%d.%d.0", $1, $2 }') \
-    git://git.kernel.org/pub/scm/network/iproute2/iproute2.git "$DEST"
+    git clone \
+        --depth 1 --single-branch \
+        -b $(uname -r | awk -F '.' '{ printf "v%d.%d.0", $1, $2 }') \
+        git://git.kernel.org/pub/scm/network/iproute2/iproute2.git "$DEST"
 fi
 pktsch_h="${DEST}/include/uapi/linux/pkt_sched.h"
 if [ ! -f "$pktsch_h" ]; then
@@ -19,13 +19,19 @@ fi
 pushd "$DEST"
 git checkout "include/"
 popd
-cat >> "$pktsch_h" << EOF
+cat >>"$pktsch_h" <<EOF
 #ifndef DUALPI2_DEV
 #include "${HERE}/../kernel_modules/sch_dualpi2/compat-pkt_sched.h"
 #else
 #include "${HERE}/../kernel_modules/sch_dualpi2_dev/compat-pkt_sched.h"
 #endif
 EOF
+
+utils_h="${DEST}/include/utils.h"
+if [ -f "$utils_h" ]; then
+    sed -i '281 i \\n#ifndef max\n#define max(a,b) \\\n  ({ __typeof__ (a) _a = (a); \\\n__typeof__ (b) _b = (b); \\\n_a > _b ? _a : _b; })\n#endif' $utils_h
+fi
+
 for qdisc in ${HERE}/*.c; do
     qdisc_o="$(basename $qdisc)"
     qdisc_o="${qdisc_o/%.c/.o}"
