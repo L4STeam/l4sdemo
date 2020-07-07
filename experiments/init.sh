c4c4dd063d2497e2e40aafce67725a514b342fd8
Olga Albisser <olga@albisser.org>
2020-07-07 01:15:56 -0700

    fixed some bugs in init.sh

--- experiments/init.sh
+++ experiments/init.sh
@@ -1,4 +1,5 @@
 pushd ../
+source environment.sh
 ./setup_endhosts.sh
 # build and load qdisc modules
 ./qdisc_modules_init.sh
@@ -8,16 +9,16 @@ popd
 
 # copy scripts required for experiments
 for client in "$CLIENT_A" "$CLIENT_B"; do
-	scp sh/*set_delay_port_netem.sh $client/.
+	scp sh/*set_delay_port_netem.sh $client:.
 done
 
 # build traffic analyzer
-pushd traffic_analyzer
+pushd ../traffic_analyzer
 make -j$(nproc)
 popd
 
 # build stats tools
-pushd stats
+pushd ../stats
 make
 popd
 
